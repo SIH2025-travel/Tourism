eda1ef9c218791464abefb29dd76adeac82a2340
Ramita Bala <mithubala1211@gmail.com>
2025-09-13 13:39:35 +0530

    Fix merge conflict in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,7 @@
-<<<<<<< HEAD
-// App.jsx
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import LoginPage from "./components/LoginPage";
-import SignUp from "./components/SignUp";
-=======
-// import React from "react";
-// import LoginPage from "./components/LoginPage"; // Importing your component
-
-// function App() {
-//   return (
-//     <div>
-//       <LoginPage />   {/* Using the component */}
-//     </div>
-    
-//   );
-// }
-
-// export default App;
-
-import React from "react";
-import SignUp from "./components/SignUp"; // Importing your component
->>>>>>> 2285b470e1f8192dc8de1a095cf72b0427f80348
+import SignUp from "./components/signUp"; // Capitalized to match component usage
 
 function App() {
   return (
